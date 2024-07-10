6bca58ab5ed75fa27b3dd128bfd3fb9c6b67189f
Aurio Rajaa <riorajaa2018@gmail.com>
2024-07-11 00:08:49 +0700

    add: edit and delete function

--- database-config.js
+++ database-config.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
          .then(() => {
             // Menampilkan pesan sukses
             alert("Article created successfully!");
-            window.location.reload();
+            window.location.href = 'admin-home.html';
          })
          .catch((error) => {
             // Menampilkan pesan error
@@ -99,6 +99,61 @@ document.addEventListener("DOMContentLoaded", () => {
       createArticle();
    });
 });
+
+// PROSES MENAMPILKAN ARTIKEL DI HALAMAN HOME
+document.addEventListener("DOMContentLoaded", () => {
+   // Mendapatkan referensi tabel yang diinginkan dari database
+   const articleRef = databaseRef(db, "Articles");
+
+   // Mendapatkan elemen HTML yang akan dimanipulasi
+   const displayArticleContainer = document.querySelector("#display-recent-article");
+
+   // Mendapatkan data artikel untuk ditampilkan di halaman home
+   onValue(articleRef, (snapshot) => {
+      // Mendapatkan data article
+      const data = snapshot.val();
+
+      // Menghapus konten yang ada (untuk mencegah duplikasi)
+      displayArticleContainer.innerHTML = "";
+
+      // Mengubah objek data menjadi array untuk diurutkan
+      const dataArray = Object.entries(data);
+
+      // Mengurutkan artikel berdasarkan tanggal pembuatan (CreatedAt) dari yang terbaru
+      dataArray.sort((a, b) => {
+         const dateA = new Date(a[1].CreatedAt);
+         const dateB = new Date(b[1].CreatedAt);
+         return dateB - dateA;
+      });
+
+      // Menampilkan maksimal 6 artikel terbaru
+      const maxArticles = 6;
+      const displayedArticles = dataArray.slice(0, maxArticles);
+
+      // Melakukan iterasi setiap entri di dalam data yang sudah diurutkan
+      displayedArticles.forEach(([uid, articleData]) => {
+         const articleCard = document.createElement("div");
+         articleCard.className = "flex items-center";
+
+         // Menampilkan elemen HTML
+         articleCard.innerHTML = `
+            <a href="admin-article.html?uid=${uid}" class="flex items-center">
+               <img src="${articleData.ArticleImage}" alt="" class="object-cover w-24 h-24 mr-4">
+               <div>
+                  <h1 class="text-lg font-bold">${articleData.ArticleTitle}</h1>
+                  <p class="text-sm text-gray-600 italic">${new Date(articleData.CreatedAt).toLocaleDateString()}</p>
+               </div>
+            </a>
+         `;
+
+         // Menambahkan elemen HTML ke dalam div parents
+         displayArticleContainer.appendChild(articleCard);
+      });
+   }, (errorObject) => {
+      console.log("Error getting data: " + errorObject.code);
+   });
+});
+
 
 // PROSES MENAMPILKAN ARTIKEL DI HALAMAN HOME
 document.addEventListener("DOMContentLoaded", () => {
@@ -150,8 +205,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
          // Menampilkan elemen HTML
          articleCard.innerHTML = `
-               <a href="admin-article.html?uid=${uid}" class="hover:border-b hover:border-red-500 flex flex-col gap-2 py-2 transform transition-transform duration-500 ease-in-out hover:scale-110">
-                   <img src="${articleData.ArticleImage}" alt="" class="rounded-md object-cover w-full h-56">
+               <a href="admin-article.html?uid=${uid}" class="hover:border-y hover:border-red-500 flex flex-col gap-2 py-2">
+                   <img src="${articleData.ArticleImage}" alt="" class="object-cover w-full h-56">
                    <h1 class="text-lg font-bold tracking-tight pt-2">${articleData.ArticleTitle}</h1>
                    <div class="flex flex-row justify-between">
                        <p class="text-sm text-gray-600 italic">${articleData.ArticleCategory}</p>
@@ -169,146 +224,250 @@ document.addEventListener("DOMContentLoaded", () => {
    });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// FUNGSI MENAMPILKAN DAFTAR KATEGORI DI HOME PAGE
+// PROSES MENAMPILKAN ARTIKEL DI HALAMAN ARTIKEL DETAIL
 document.addEventListener("DOMContentLoaded", () => {
-   // Referensi tabel Categories di Firebase Realtime Database
-   const categoryRef = databaseRef(db, "Categories");
 
-   // Mendapatkan elemen ul untuk menambahkan kategori
-   const categoryList = document.getElementById("category-list");
+   // Membuat fungsi untuk mendapatkan query dari URL
+   function getQueryParam(param) {
+      // Menyimpan URL saat ini ke dalam variabel
+      const urlParams = new URLSearchParams(window.location.search);
 
-   // Mendapatkan data kategori dari Firebase Realtime Database
-   onValue(categoryRef, (snapshot) => {
-      // Mendapatkan data kategori
-      const categories = snapshot.val();
+      // Memberikan value dari function getQueryParam menjadi URL yang sedang dibuka
+      return urlParams.get(param);
+   }
 
-      // Menghapus kategori yang ada (kecuali "All") untuk mencegah duplikasi
-      categoryList.innerHTML = '<li data-filter="all" class="list active cursor-pointer focus:outline-none focus:ring-4 font-medium text-md py-2">All</li>';
+   function displayComments(uid) {
+      const commentsRef = databaseRef(db, `Articles/${uid}/Comments`);
 
-      // Melakukan iterasi setiap entri di dalam data kategori
-      for (const key in categories) {
-         const category = categories[key];
+      get(commentsRef).then((snapshot) => {
+         const commentsList = document.getElementById("comments-list");
+         commentsList.innerHTML = ""; // Clear previous comments
 
-         const categoryItem = document.createElement("li");
-         categoryItem.dataset.filter = category.toLowerCase(); // Pastikan data-filter sesuai dengan kategori
-         categoryItem.className = "list cursor-pointer capitalize hover:underline focus:outline-none focus:ring-4 font-medium text-md py-2";
-         categoryItem.textContent = category;
-         categoryList.appendChild(categoryItem);
-      }
-   }, (errorObject) => {
-      console.log("Error getting data: " + errorObject.code);
-   });
+         if (snapshot.exists()) {
+            const commentsData = snapshot.val();
+
+            // Iterate through comments and display them
+            for (const commentKey in commentsData) {
+               const comment = commentsData[commentKey];
+               const commentElement = document.createElement("li");
+               commentElement.classList.add("py-2", "border-b", "border-gray-200");
+               commentElement.innerHTML = `
+                      <p class="text-sm text-gray-500">Comment by: ${comment.displayName}</p>
+                      <p class="text-gray-800 mb-2">${comment.text}</p>
+                      <button class="text-red-500 hover:text-red-700 delete-comment-btn" data-comment-id="${commentKey}">Delete</button>
+                  `;
+               commentsList.appendChild(commentElement);
+            }
+
+            // Attach event listener for delete comment buttons
+            const deleteCommentButtons = document.querySelectorAll(".delete-comment-btn");
+            deleteCommentButtons.forEach(button => {
+               button.addEventListener("click", (event) => {
+                  const commentId = event.target.dataset.commentId;
+                  const confirmDelete = confirm("Are you sure you want to delete this comment?");
+                  if (confirmDelete) {
+                     deleteComment(uid, commentId);
+                  }
+               });
+            });
+         } else {
+            const noCommentsElement = document.createElement("p");
+            noCommentsElement.classList.add("text-gray-500", "italic");
+            noCommentsElement.textContent = "Tidak ada komen untuk saat ini";
+            commentsList.appendChild(noCommentsElement);
+         }
+      }).catch((error) => {
+         console.log("Error getting comments: " + error);
+      });
+   }
+
+
+   // Fungsi untuk mendapatkan artikel berdasarkan UID
+   function getArticle(uid) {
+      const articleRef = databaseRef(db, `Articles/${uid}`);
+
+      return get(articleRef);
+   }
+
+   // Mendapatkan referensi UID Artikel yang sedang dibuka dari URL
+   const uid = getQueryParam("uid");
+
+   // Pengkondisian untuk menampilkan artikel yang sedang dibuka berdasarkan UID nya
+   if (uid) {
+      // Mendapatkan artikel
+      getArticle(uid).then((snapshot) => {
+         // Pengkondisian untuk menampilkan artikel jika UID yang diinginkan ada di tabel
+         if (snapshot.exists()) {
+            const articleData = snapshot.val();
+
+            document.getElementById("article-image").src = articleData.ArticleImage;
+            document.getElementById("article-title").innerText = articleData.ArticleTitle;
+            document.getElementById("article-category").innerText = articleData.ArticleCategory;
+            document.getElementById("article-content").innerText = articleData.ArticleContent;
+            document.getElementById("article-created-at").innerText = new Date(articleData.CreatedAt).toLocaleDateString();
+
+            // Mendapatkan elemen Edit This Article di navbar
+            const editArticleLink = document.querySelector("#edit-article");
+
+            // Merubah href dari tag di navbar menjadi URL yang diinginkan
+            if (editArticleLink) {
+               editArticleLink.href = `admin-edit-article.html?uid=${uid}`;
+            }
+
+            // Tampilkan komentar
+            displayComments(uid);
+         } else {
+            console.log("No data available for this article");
+         }
+      }).catch((error) => {
+         console.log("Error getting article data: " + error);
+      });
+   } else {
+      console.log("No UID available for this article");
+   }
+
+   // Fungsi untuk menghapus komentar berdasarkan UID artikel dan UID komentar
+   function deleteComment(articleUid, commentUid) {
+      const commentRef = databaseRef(db, `Articles/${articleUid}/Comments/${commentUid}`);
+      remove(commentRef).then(() => {
+         alert("Comment deleted successfully");
+         // Refresh comments after deletion
+         window.location.reload();
+      }).catch((error) => {
+         console.error("Error deleting comment: ", error);
+      });
+   }
+
 });
 
-
-// FUNGSI MENAMPILKAN PILIHAN KATEGORI DI ADD ARTIKEL
+// PROSES MENAMPILKAN ARTIKEL DI HALAMAN EDIT ARTIKEL
 document.addEventListener("DOMContentLoaded", () => {
    // Mendapatkan referensi tabel Categories dari Firebase
    const categoryRef = databaseRef(db, "Categories");
 
    // Mendapatkan elemen select untuk menambahkan kategori
-   const categorySelect = document.getElementById("article-category");
+   const categorySelect = document.getElementById("category-article-input");
 
-   // Mendapatkan data kategori dari tabel Categories
-   onValue(categoryRef, (snapshot) => {
-      // Menghapus kategori yang ada kecuali "Select Category" untuk mencegah duplikasi
-      categorySelect.innerHTML = '<option>Pilih Kategori</option>';
+   // Mendapatkan elemen HTML lainnya
+   const displayCurrentThumbnailImage = document.querySelector("#display-thumbnail-image");
+   const articleUIDInput = document.querySelector("#article-uid");
+   const articleTitleInput = document.querySelector("#article-title-input");
+   const articleContentInput = document.querySelector("#content-article-input");
+   const articleImageInput = document.querySelector("#file_input_edit");
+   const updateArticleButton = document.querySelector("#edit-article-btn");
+   const deleteArticleButton = document.querySelector("#delete-article-btn");
 
-      // Memeriksa apakah data snapshot ada
-      if (snapshot.exists()) {
-         const data = snapshot.val();
+   // Mendapatkan UID dari query parameter URL
+   function getQueryParam(param) {
+      const urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get(param);
+   }
 
-         // Melakukan iterasi setiap entri di dalam data
-         for (const key in data) {
-            if (data.hasOwnProperty(key)) {
-               let category = data[key];
-               category = capitalizeWords(category); // Capitalize category
+   const uid = getQueryParam("uid");
 
-               // Membuat elemen option untuk setiap kategori
-               const categoryOption = document.createElement("option");
-               categoryOption.value = category;
-               categoryOption.textContent = category;
-               categorySelect.appendChild(categoryOption);
-            }
+   if (uid) {
+      const articleRef = databaseRef(db, `Articles/${uid}`);
+
+      get(articleRef).then((snapshot) => {
+         if (snapshot.exists()) {
+            const articleData = snapshot.val();
+            categorySelect.value = articleData.ArticleCategory;
+            articleUIDInput.value = uid;
+            articleTitleInput.value = articleData.ArticleTitle;
+            simplemde.value(articleData.ArticleContent);
+            displayCurrentThumbnailImage.src = articleData.ArticleImage;
+         } else {
+            console.log("No data available");
          }
-      } else {
-         console.log("No data available in Categories");
-      }
-   }, (errorObject) => {
-      console.error("Error getting data: " + errorObject.code);
-   });
-});
+      }).catch((error) => {
+         console.log("Error getting data: ", error);
+      });
+   } else {
+      console.log("No UID provided");
+   }
 
-// Fungsi untuk capitalizing kategori
-function capitalizeWords(str) {
-   return str.replace(/\b\w/g, char => char.toUpperCase());
-}
+   // Fungsi untuk update artikel
+   function updateArticle() {
+      const imageFile = articleImageInput.files[0];
+      const articleRef = databaseRef(db, `Articles/${articleUIDInput.value}`);
 
-document.addEventListener("DOMContentLoaded", () => {
-   // Referensi elemen input dan tombol
-   const addCategoryBtn = document.getElementById("add-category-btn");
-   const newCategoryInput = document.getElementById("new-category");
-
-   // Referensi ke tabel Categories di Firebase Realtime Database
-   const categoryRef = databaseRef(db, "Categories");
-
-   addCategoryBtn.addEventListener("click", () => {
-      const newCategory = newCategoryInput.value.trim();
-
-      if (newCategory) {
-         // Cek apakah kategori sudah ada di database
-         get(categoryRef).then((snapshot) => {
-            const data = snapshot.val();
-            const existingCategories = Object.values(data || {});
-
-            // Ubah kategori yang sudah ada menjadi huruf kecil
-            const lowerCaseExistingCategories = existingCategories.map(category => category.toLowerCase());
-
-            // Ubah kategori baru menjadi huruf kecil
-            const lowerCaseNewCategory = newCategory.toLowerCase();
-
-            // Cek apakah kategori baru sudah ada dalam daftar
-            if (lowerCaseExistingCategories.includes(lowerCaseNewCategory)) {
-               alert("Category already exists!");
-            } else {
-               // Tambahkan kategori baru dengan menggunakan push untuk menghasilkan UID otomatis
-               push(categoryRef, newCategory)
-                  .then(() => {
-                     alert("Category added successfully!");
-                     newCategoryInput.value = ''; // Clear the input field
-                  })
-                  .catch((error) => {
-                     console.error("Error adding category:", error);
-                  });
-            }
+      function updateRecipeData(imageURL) {
+         update(articleRef, {
+            ArticleTitle: articleTitleInput.value,
+            ArticleCategory: categorySelect.value,
+            ArticleContent: articleContentInput.value,
+            ArticleImage: imageURL || displayCurrentThumbnailImage.src
+         }).then(() => {
+            alert("Article updated successfully!");
+            window.location.href = "admin-home.html";
          }).catch((error) => {
-            console.error("Error checking categories:", error);
+            console.error("Error updating article: ", error);
+         });
+      }
+
+      if (imageFile) {
+         const storageImageRef = storageRef(storage, `Images/${imageFile.name}`);
+         uploadBytes(storageImageRef, imageFile).then(() => {
+            return getDownloadURL(storageImageRef);
+         }).then((imageURL) => {
+            updateRecipeData(imageURL);
+         }).catch((error) => {
+            console.error("Error uploading image: ", error);
          });
       } else {
-         alert("Please enter a category name.");
+         updateRecipeData();
+      }
+   }
+
+   // Event listener untuk tombol update
+   updateArticleButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      updateArticle();
+   });
+
+   // Fungsi untuk hapus artikel
+   function deleteArticle() {
+      const articleRef = databaseRef(db, `Articles/${articleUIDInput.value}`);
+
+      get(articleRef).then((snapshot) => {
+         if (snapshot.exists()) {
+            const articleData = snapshot.val();
+            const imageURL = articleData.ArticleImage;
+
+            remove(articleRef).then(() => {
+               alert("Article deleted successfully!");
+
+               if (imageURL) {
+                  const imageName = decodeURIComponent(imageURL.split('/o/')[1].split('?')[0]);
+                  const storageImageRef = storageRef(storage, imageName);
+
+                  deleteObject(storageImageRef).then(() => {
+                     console.log("Image deleted successfully from storage.");
+                  }).catch((error) => {
+                     console.error("Error deleting image from storage: ", error);
+                  });
+               }
+
+               window.location.href = "admin-home.html";
+            }).catch((error) => {
+               console.error("Error deleting article: ", error);
+               alert("Failed to delete article. Please try again.");
+            });
+         } else {
+            console.log("No data available for the given UID.");
+            alert("Article not found. Please check the UID.");
+         }
+      }).catch((error) => {
+         console.error("Error retrieving article: ", error);
+      });
+   }
+
+   // Event listener untuk tombol delete
+   deleteArticleButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      const confirmDelete = confirm("Are you sure you want to delete this article? This process can't be undone.");
+      if (confirmDelete) {
+         deleteArticle();
       }
    });
 });
